30eee89a10365e3c14ebb699b2d9832b27e05103
farrel <muhamadfarrelaqil@gmail.com>
2025-05-21 10:40:57 +0700

    Fix

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
-    base: process.env.VITE_BASE_PATH || "/UPRAK-Farrelaqil"
+    
   ],
+  base: process.env.VITE_BASE_PATH || "/UPRAK-Farrelaqil"
 })
